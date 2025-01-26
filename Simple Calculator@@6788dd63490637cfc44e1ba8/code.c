f47463abb6425ca7ceb92dd9ca24f030e05018f2
3423krish <krish549yadav@gmail.com>
2025-01-26 12:25:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 int main() {
-    int a,b,d,e,f,g;
-    char c;
-    scanf("%d %d %c",&a,&b,&c);
-    d=a+b;
-    e=a-b;
-    f=a*b;
-    g=a/b;
-    if (c =='+'){
-        printf("%d",(d));
-    }else if(c =='-'){
-        printf("%d",(e));
-    }else if(c =='*'){
-        printf("%d",(f));
-    }else if(c =='/'){
-        printf("%d",(g));
-    }else{
-        printf("error");
+    int a,b;
+    char operator;
+    scanf("%d %d %c",&a,&b,&operator);
+    switch (operator){
+        case '+':
+            printf("%d",a+b);
+            break;
+        case '-':
+            printf("%d",a-b);
+            break;
+        case '*':
+            printf("%d",a*b);
+            break;
+        case '/':
+            if (b==0){
+                printf("Can not be divisible by 0")
+            }else{
+                printf("%d",a/b);
+            }
+            break;
+        default:
+        printf("Error");      
     }
     return 0;
 }
