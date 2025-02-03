98ea511c78034368884a62c8ea8231cafc49cf3e
3423krish <krish549yadav@gmail.com>
2025-02-03 22:33:11 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -15,7 +15,7 @@ int main() {
         scanf("%d %s %f",&S[i].roll,&S[i].name,&S[i].marks);
     }
     for(int i=o;i<n;i++){
-        printf("%d %s %.2f ")
+        printf("%d %s %.2f ",S[i].roll,S[i].name,S[i].marks);
     }
     return 0;
 }
